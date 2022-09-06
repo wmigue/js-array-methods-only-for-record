baac71c0cbe0d8b43064586bb2a5a9dedd9256d7
unknown <wmigue@gmail.com>
2022-09-06 12:08:49 -0300

    6-9-2022

--- filter.js
+++ filter.js
@@ -5,3 +5,5 @@ const resultado = words.filter(x => x.length > 6)
 console.log(resultado)
 // return: Array ["exuberant", "destruction", "present"]
 //crea un nuevo array con todos los elementos que cumplan la condición implementada por la función dada.
+//si no hay coincidencias, retorna un array vacio []
+//con resultado.length vemos la longitud del array devuelto.
--- find.js
+++ find.js
@@ -1,7 +1,6 @@
-const arr = [5, 12, 8, 130, 44];
-
-const resultado = arr.find(x => x > 10);
-
-console.log(resultado);
-// devuelve el valor del primer elemento del array que cumple la función de prueba proporcionada.
+const arr = [5, 12, 8, 130, 44]
+const resultado = arr.find(x => x > 10)
+console.log(resultado)
+// devuelve el valor del primer elemento del array que cumple la condicion.
 // return: 12
+//si no hay coincidencias, retorna undefined
